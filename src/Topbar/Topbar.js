9b37cc356e7ce9e3eec0bff24d57f2e3e096c446
Parth Prakhar Mishra <parthprakharmishra@gmail.com>
2021-07-11 13:43:10 +0530

    Add files via upload

---
+++ src/Topbar/Topbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Topbar.module.css';
+
+const Topbar=()=>{
+    return(
+        <header className="App-header">
+            <nav className={classes.Topbar}>
+            <img src="https://i.dlpng.com/static/png/197684_preview.png" alt="Amazon Logo"/>
+            </nav>
+        </header>
+    )
+}
+
+export default Topbar;
